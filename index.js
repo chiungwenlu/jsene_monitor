57612919b35ca378fd1f0a258b77c55ce628fbd8
chiungwenlu <chiungwenlu@gmail.com>
2024-09-15 03:21:22 +0800

    one page web scraper

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const app = express();
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  try {
+    const page = await browser.newPage();
+
+    // 導航到目標網站
+    await page.goto("https://developer.chrome.com/");
+
+    // 抓取網站的 <title> 標籤內容
+    const fullTitle = await page.title();
+
+    // 印出完整標題
+    const logStatement = `The title of this page is: ${fullTitle}`;
+    console.log(logStatement);
+
+    // 將標題回傳到用戶端
+    res.send(logStatement);
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
 });
 
 app.listen(PORT, () => {
